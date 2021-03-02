43c8ce869f46cd6911c08cbc6187a6c54149e26f
Jessica Anyanwu <jesax013@gmail.com>
2021-03-02 13:56:08 +0100

    Add all products under a category to cart

--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -1,8 +1,13 @@
 import { hooks } from '@bigcommerce/stencil-utils';
 import CatalogPage from './catalog';
+import CartPage from './cart';
 import compareProducts from './global/compare-products';
 import FacetedSearch from './common/faceted-search';
 import { createTranslationDictionary } from '../theme/common/utils/translations-utils';
+import swal from './global/sweet-alert';
+import { bind, get } from 'lodash';
+import utils from '@bigcommerce/stencil-utils';
+import { showAlertModal } from './global/modal';
 
 export default class Category extends CatalogPage {
     constructor(context) {
@@ -11,12 +16,16 @@ export default class Category extends CatalogPage {
     }
 
     onReady() {
+        this.$overlay = $('[data-category] .loadingOverlay')
+            .hide();
         $('[data-button-type="add-cart"]').on('click', (e) => {
             $(e.currentTarget).next().attr({
                 role: 'status',
                 'aria-live': 'polite',
             });
         });
+
+        this.bindEvents()
 
         compareProducts(this.context.urls);
 
@@ -88,6 +97,126 @@ export default class Category extends CatalogPage {
                 maxPriceNotEntered,
                 onInvalidPrice,
             },
+        });
+    }
+
+    addAllProductsToCart(event) {
+        const $addAllToCartBtn = $('[data-add-all]#form-action-addToCart');
+        const originalBtnVal = $addAllToCartBtn.val();
+        const waitMessage = $addAllToCartBtn.data('waitMessage');
+        const {cartId, categoryProducts} = this.context;
+        const successMsg = "Products successfully added to cart";
+        const products = categoryProducts.filter(product => !product.has_options);
+        const cartQuantity = parseInt($('.cart-quantity').text());
+
+        // Do not do AJAX if browser doesn't support FormData
+        if (window.FormData === undefined) {
+            return;
+        }
+
+        // Prevent default
+        event.preventDefault();
+
+        if (!products.length) {
+            return swal.fire({
+                text: "You have no item without variant in this category. Select a product and pick a variant to add",
+                icon: 'info'
+            });
+        }
+
+        $addAllToCartBtn
+            .val(waitMessage)
+            .prop('disabled', true);
+        this.$overlay.show();
+
+        const errorAction = (error) => {
+            const tmp = document.createElement('DIV');
+            tmp.innerHTML = error;
+            $addAllToCartBtn
+                .val(originalBtnVal)
+                .prop('disabled', false);
+            this.$overlay.hide();
+            return showAlertModal(tmp.textContent || tmp.innerText);
+        }
+
+        //Recursively add all items to cart
+        const addItems = (formData, index) => {
+            utils.api.cart.itemAdd(formData, (err, response) => {
+                const errorMessage = err || response.error;
+                if (errorMessage) {
+                    errorAction(errorMessage);
+                }
+
+                if (index == products.length-1) {
+                    swal.fire({
+                        text: successMsg,
+                        icon: 'success',
+                    }).then(() => {
+                        window.location = response.data.cart_item.cart_url || this.context.urls.cart;
+                    })
+                } else {
+                    index++;
+                    const formData = new FormData();
+                    formData.append('product_id',  products[index].id);
+                    addItems(formData, index);
+                }
+            })
+        } 
+
+        // Add items to cart
+        if(!cartQuantity) {
+            const formData = new FormData();
+            formData.append('product_id',  products[0].id);
+            addItems(formData, 0)
+        } else {
+            fetch(`https://ligk183p53.execute-api.us-east-2.amazonaws.com/default/cart/items?cart_id=${cartId}`,
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        line_items: products.map(product => ({
+                            quantity: 1,
+                            product_id: product.id,
+                            list_price: get(product, 'price.without_tax.value'),
+                        }))
+                    })
+                }
+            ).then(() => {
+                $addAllToCartBtn
+                    .val(originalBtnVal)
+                    .prop('disabled', false);
+                this.$overlay.hide();
+
+                swal.fire({
+                    text: successMsg,
+                    icon: 'success',
+                }).then(() => {
+                    window.location = this.context.urls.cart
+                });
+            }).catch(err => {
+                errorAction(err);
+            })
+        }
+
+        $addAllToCartBtn.next().attr({
+            role: 'status',
+            'aria-live': 'polite',
+        });
+    }
+
+    bindEvents() {
+        const addAllProductsToCart = bind(this.addAllProductsToCart, this);
+
+        $('[data-add-all]#form-action-addToCart').on('click', event => {
+            swal.fire({
+                text: "Only items without variants will be added to cart",
+                icon: 'info',
+                showCancelButton: true,
+            }).then((result) => {
+                if (result.value) {
+                    addAllProductsToCart(event);
+                }
+            });
+            event.preventDefault();
         });
     }
 }
